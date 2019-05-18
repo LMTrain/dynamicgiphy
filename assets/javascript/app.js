17ada2f37589a322f3e4b0bb2501110a7ff08dab
LM Train <layconmm@msn.com>
2019-05-17 23:15:43 -0500

    modify and add line of codes to animate and still gif image

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,25 +1,15 @@
 
   // Initial array of artists
-  var artists = ['Tupac', 'Notorious B.I.G', 'Jay Z', 'Outkast', 'Nicki Minaj', 'R. Kelly', 'Beyonce', 'Aaliya'];
+  var artists = ['Tupac', 'Notorious B.I.G', "Faith Evans",'Jay Z', 'Outkast', 'Nicki Minaj', 'Drake', 'DJ Khaled', 'Chris Brown', 'Akon', 'R. Kelly', 'Beyonce', 'Aaliya', "Tyrese", "Luther Vandross", "Tina Turner", "Boyz II Men", "Usher", "Bobby Brown", "Cardi B"];
 
   // displayartistInfo function re-renders the HTML to display the appropriate content
   function displayartistInfo() {
 
     var artist = $(this).attr("data-name");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + "&api_key=HEdbTT0vdsath3C9VzHnGKf0KeRx3w50&q=" + artist + "&limit=10";
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + "&api_key=HEdbTT0vdsath3C9VzHnGKf0KeRx3w50&q=" + artist + "&limit=10&offset=0&rating=R&lang=en";
+    // "https://api.giphy.com/v1/gifs/search?api_key=HEdbTT0vdsath3C9VzHnGKf0KeRx3w50&q=tupac&limit=10&offset=0&rating=R&lang=en"
 
-    // $(".gif").on("click", function () {
-     
-    //   var state = $(this).attr("data-state");
-     
-    //   if (state === "still") {
-    //       $(this).attr("src", $(this).attr("data-animate"));
-    //       $(this).attr("data-state", "animate");
-    //   } else {
-    //       $(this).attr("src", $(this).attr("data-still"));
-    //       $(this).attr("data-state", "still");
-    //   }
-    // });
+    
 
     // Creating an AJAX call for the specific artist button being clicked
     $.ajax({
@@ -29,20 +19,40 @@
 
       // Creating a div to hold the artist
         var results = response.data;
+        
+        $("#artists-view").empty();
+
         for (var i = 0; i < results.length; i++) {
 
             // Creating and storing a div tag
             var artistDiv = $("<div class='artist'>");
-            // var titleDiv = $("<div class='artist'>");
+            // var titleDiv = $("<div class='artists'>");
+            
+            var p = $("<p>").text(results[i].title);
             // var p = $("<p>").text("Rating: " + results[i].title);
-            // p = p.attr("<p>", results[i].title);
+            // p = p.attr("artists-title", results[i].title);
             
             var image = $("<img>");
-            
-            image.attr("src", results[i].images.fixed_width_small_still.url);               
-            // titleDiv.append(p);     
+            image.attr("src", results[i].images.fixed_height_still.url); 
+            image.attr("data-still", results[i].images.fixed_height_still.url);
+            image.attr("data-animate", results[i].images.fixed_height_downsampled.url);
+            image.attr("data-state", "still");
+            image.on("click", function () {
+              console.log(state);
+             
+              var state = $(this).attr("data-state");
+             
+              if (state === "still") {
+                  $(this).attr("src", $(this).attr("data-animate"));
+                  $(this).attr("data-state", "animate");
+              } else {
+                  $(this).attr("src", $(this).attr("data-still"));
+                  $(this).attr("data-state", "still");
+              }
+            });              
+            artistDiv.append(p);     
             artistDiv.append(image);
-            // $("#artists-title").prepend(titleDiv);
+            // $("#artists-title").prepend(artistDiv);
             $("#artists-view").prepend(artistDiv);
         }      
     });
